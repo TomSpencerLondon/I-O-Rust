96101d50bb0e41afe95e048c9844905c8d6f26e7
Tom <thomas.spencer@codurance.com>
2021-04-26 19:25:22 +0100

    add dyn Error

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,13 +14,22 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box< dyn error::Error>> {
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
 
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("with text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
