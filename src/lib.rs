8bab9cc0bc1ce29c9629a8df88b46a59de78ceeb
Tom <thomas.spencer@codurance.com>
2021-04-26 19:40:29 +0100

    add passing test for search

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,38 @@ use std::error;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        )
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 pub fn run(config: Config) -> Result<(), Box< dyn error::Error>> {
     let mut f = File::open(config.filename)?;
     let mut contents = String::new();
